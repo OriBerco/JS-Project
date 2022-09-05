503d99f7d7c5bc45c03cef1d9cd95d19ba9a4d08
OriBerco <Oriberco@gmail.com>
2022-09-05 05:09:45 +0300

    hi

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ function createTask() {
         taskAddDescription,
         taskAddEndDate
     )
+    if (/\d/.test(taskAddName) == true){
+        return alert("Name cannot contain numbers")
+    }
     if (tasks.some(t => t.name == taskAddName)) {
         return alert("Task already exists")
     }
@@ -66,14 +69,18 @@ function createTask() {
     handleTasks(tasks)
 };
 
+const a = window.localStorage.getItem('tasks');
+console.log(a);
 function handleTasks(data) {
     const container = document.getElementById('taskList');
     let html = '';
     data.forEach(t => {
+
+        window.localStorage.setItem('tasks',JSON.stringify(t));
             html += `<tr ${t.overdue()} style="display:flex">
         <td> <h2 > ${t.getName()} </h2></td>
         <td><p> ${t.getDesciption()} </p></td>
-        <td><h3>expires:${t.getEndDate()}</h3></td>
+        <td><h3>${t.getEndDate()}</h3></td>
         <td> ${t.getStatus()}</td></tr><tr><td><button onClick=deleteTask(${t.getId()})>Delete</button></td><td><button onClick=completeTask(${t.getId()})>Complete</button></td>
         <td><button id="updateBoxOpener" onClick=openUpdateTask(${t.getId()})>Update</button></td></tr>`;
 
@@ -156,9 +163,29 @@ openButton.addEventListener('click', () => {
 document.getElementById('close').addEventListener('click', () => {
     taskBox.style.display = "none";
 })
+function selectTask(){
+    const taskSelector = document.getElementById('taskSelector').value;
 
+    if (taskSelector == 'All'){
+        handleTasks(tasks);
+    }
+    if (taskSelector == 'Completed'){
+        const completedTasks = tasks.filter((t)=> {
+            return t.status == true;
+        })
+        handleTasks(completedTasks)
+    }
+    if (taskSelector == 'Uncompleted'){
+        const uncompletedTasks = tasks.filter((t) => {
+            return t.status == false;
+        })
+        handleTasks(uncompletedTasks)
+    }
+}
 
 
 /* console.log(
 tasks.push(JSON.parse(localStorage.getItem("tasks")) || []));
 console.log(tasks); */
+selectTask()
+ 
